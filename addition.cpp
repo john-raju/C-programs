1a125107b0c134057f514622001982c3d4117bce
pandu362 <125708252+pandu362@users.noreply.github.com>
2023-02-18 05:32:01 +0530

    Add files via upload

---
+++ addition.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+int a,b;
+scanf("%d%d",&a,&b);
+printf("addition %d\n",(a+b));
+printf("substraction %d\n",(a-b));
+printf("multiplication %d\n",(a*b));
+printf("division %d\n",(a/b));
+printf("modular division %d\n",(a%b));
+}
