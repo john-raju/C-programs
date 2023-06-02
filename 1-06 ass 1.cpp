5a1f44b6699571480f2be8478352227351f3439c
John Raju <125708252+john-raju@users.noreply.github.com>
2023-06-02 07:34:31 +0530

    Add files via upload

---
+++ 1-06 ass 1.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+// Function to swap two elements
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Function to perform selection sort
+void selectionSort(int arr[], int n) {
+    int i, j, minIndex;
+
+    // Traverse through the entire array
+    for (i = 0; i < n - 1; i++) {
+        minIndex = i;
+
+        // Find the index of the minimum element in the unsorted part of the array
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // Swap the minimum element with the first element of the unsorted part
+        swap(&arr[minIndex], &arr[i]);
+    }
+}
+
+// Function to print the array
+void printArray(int arr[], int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    printArray(arr, n);
+
+    selectionSort(arr, n);
+
+    printf("Sorted array: ");
+    printArray(arr, n);
+
+    return 0;
+}
+
